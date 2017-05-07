bc030ec9e078c36728edb470f2626ee499a9a579
Johannes Elmnäinen <johannes.elmnainen@cs.helsinki.fi>
2017-05-07 22:06:35 +0300

    switch routing to a class

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,33 @@
- import 'babel-polyfill';
- import React from 'react'
- import { render } from 'react-dom'
- import { Provider } from 'react-redux'
- import App from './app'
+import 'babel-polyfill';
+import React, { Component }from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './app'
 
- import Template from './containers/templateContainer'
+import Template from './containers/templateContainer'
 
- import { Router, IndexRoute, Route, Link, browserHistory } from 'react-router'
+import { Router, IndexRoute, Route, Link, browserHistory } from 'react-router'
 
 import { store, eventbus } from './store'
 
 import bind_actors from './actors'
 bind_actors(eventbus);
 
- render(
-   <Provider store={store}>
-     <Router history={browserHistory}>
-       <Route path="/" component={App}>
-         <IndexRoute component={Template} />
-       </Route>
-      </Router>
-   </Provider>,
-   document.getElementById('root')
- )
+class AppRouter extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+       <Router history={browserHistory}>
+         <Route path="/" component={App}>
+           <IndexRoute component={Template} />
+         </Route>
+        </Router>
+      </Provider>
+    )
+  }
+}
+
+render(
+  <AppRouter />,
+  document.getElementById('root')
+)
