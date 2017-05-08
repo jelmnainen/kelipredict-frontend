aeede76e8fc4e0501f1b1b04b5f150e09ceb830e
Johannes Elmnäinen <johannes.elmnainen@cs.helsinki.fi>
2017-05-08 16:29:53 +0300

    switch to react-router4

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,23 @@
-import 'babel-polyfill';
+import 'babel-polyfill'
 import React, { Component }from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import App from './app'
-
 import Template from './containers/templateContainer'
 
-import { Router, IndexRoute, Route, Link, browserHistory } from 'react-router'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import { store, eventbus } from './store'
 
 import bind_actors from './actors'
-bind_actors(eventbus);
+bind_actors(eventbus)
 
 class AppRouter extends Component {
   render() {
     return (
       <Provider store={store}>
-       <Router history={browserHistory}>
-         <Route path="/" component={App}>
-           <IndexRoute component={Template} />
-         </Route>
-        </Router>
+       <BrowserRouter>
+         <Route exact path="/" component={Template} />
+       </BrowserRouter>
       </Provider>
     )
   }
