804611e53249a6407e5a54f34f87a3bafa4f1918
Johannes Elmnäinen <johannes.elmnainen@cs.helsinki.fi>
2017-05-08 16:09:28 +0300

    switch to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,62 @@
-const webpack = require('webpack');
+const path = require('path')
 
 module.exports = {
-    entry: './frontend/src/index.js',
-    output: {
-        path: './frontend/bin',
-        filename: 'app.bundle.js'
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }]
-    },
-    plugins: [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(true),
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {
-        //         warnings: false,
-        //     },
-        //     output: {
-        //         comments: false,
-        //     },
-        // }),
-
+  entry: './frontend/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'frontend/bin'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ],
+        exclude: [
+          '/node_modules/',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+        ],
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 100000,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.json$/,
+        use: [
+          {
+            loader: 'json',
+          },
+        ],
+      },
     ]
-};
+  }
+}
